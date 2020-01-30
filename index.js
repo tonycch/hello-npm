7f1b2f528f6a6d376dbd3de62c4ce93bc4f81f03
Tony Cheung <tonycch@github.com>
2020-01-30 17:41:42 +0800

    added alerts

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ function f() {
     let x;
 }
 
-function f() {
-    x = 24;
-    let x;
-}
-
-function b() {
-	const loc = null;
-	if (dist < 10)
- 	   loc = "here";
-	else
-    	loc = "there";
-}
-
 exports.printMsg = function() {
 	console.log("Hello World!");
 	return "Hello World!";
